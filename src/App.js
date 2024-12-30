4ef548c2c8ff7f03f4d32742096ed4573c6def15
MD MIRAJ ISLAM <miraz97islam@gmail.com>
2024-12-31 00:10:21 +0600

    31-122024 updated states
    my task and my notes buttons are now functional with state

--- src/App.js
+++ src/App.js
@@ -13,10 +13,24 @@ import { v4 as uuidv4 } from 'uuid';
 
 }
 
-function VisualizedNotes({keyValue, noteVlaue, onClickTriger, className}){
+
+function MapedComponents({todoList, inputType, triggers}){
+   const tasksList = todoList.filter(task=> task.inputType === inputType);
+
+  return(
+   tasksList.map((task) => (
+    <div  className='note-item'>
+          <VisualizedNotes keyValue = {task.id} noteValue ={task.task} onClickTriger ={()=>triggers[0](task.id)} className = {'note-item-content'} />
+          <DeleteButton onClickTriger={()=> triggers[1](task.id)} className={'btn-delete'} />
+    </div> 
+    ))
+  )
+}
+
+function VisualizedNotes({keyValue, noteValue, onClickTriger, className}){
   return(
     <div key={keyValue}  onClick ={onClickTriger} className={className}>
-        {noteVlaue} 
+        {noteValue} 
     </div>
   )
 }
@@ -29,9 +43,9 @@ function Buttons({onClickTriger, className, btnTitle}){
   )
 }
 
-function DeleteButton({onClickTriiger, className}){
+function DeleteButton({onClickTriger, className}){
   return (  
-        <button onClick={onClickTriiger}  className= {className}>
+        <button onClick={onClickTriger}  className= {className}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
            class="size-4 lg:size-5 ">
               <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -45,28 +59,26 @@ function App() {
   const[newInput, setnewInput]  = useState("");
   const[todoList, setTodoList] = useState([]);
   const[editTaskId, setEditTaskId] = useState(null);
-  // const[categoryId, setCategoryId] = useState("");
   const[inputType, setinputType] = useState("elementInput");
+    
+    useEffect(() => { 
+      const savedTodos = localStorage.getItem("todoList");
 
-  let InputCompnentClassName = 'elementInput';
+      if (savedTodos) {
+        setTodoList(JSON.parse(savedTodos));
+      }
+    }, []);
 
 
-  useEffect(() => { 
-    const savedTodos = localStorage.getItem("todoList");
 
-    if (savedTodos) {
-      setTodoList(JSON.parse(savedTodos));
-    }
-  }, []);
-  
-  const addTodo = ()=>{ 
+    const addTodo = ()=>{ 
 
     if (!newInput.trim()){
       return
     }
 
      if (editTaskId){
-       todoList.find(task=> task.id===editTaskId).task = newInput;
+        todoList.find(task=> task.id===editTaskId).task = newInput;
 
         localStorage.setItem("todoList", JSON.stringify(todoList));
 
@@ -86,6 +98,9 @@ function App() {
       localStorage.setItem("todoList", JSON.stringify(updateList));
   
       setTodoList(updateList);
+
+    
+    
       setnewInput("");
     }    
   }
@@ -108,7 +123,6 @@ function App() {
 
     setTodoList(updateList);
     setnewInput("");
-
   }
 
 
@@ -117,27 +131,11 @@ function App() {
   }
   
 
-  function handleTasks(){
-    const tasksList = todoList.filter(task=> task.inputType==='elementInput');
+  function handleTabs(typeOfInput){
 
-    if(tasksList){
-      setTodoList(tasksList);
+      setinputType(typeOfInput);
+
     }
-
-    setinputType('elementInput');
-  }
-
-
-  function handleNotes(){
-
-    const notesList = todoList.filter(task=> task.inputType==='elementTextArea');
-
-    if(notesList){
-      setTodoList(notesList);
-    }
-
-    setinputType('elementTextArea');
-  }
 
 
   return (
@@ -156,25 +154,13 @@ function App() {
             </div>
         </div>
 
-        {/* <div>
-          <select name="importance" id="categoryList">
-            <option value="all">All</option>
-            <option value="high">High</option>
-            <option value="medium">Medium</option>
-            <option value="low">Low</option>
-        </select>
-        </div> */}
          <div className='tab-buttons'>
-                <button onClick={handleTasks} className='btn-tasks'>My Tasks</button>
-                <button onClick={handleNotes} className='btn-notes'>My Notes</button>
+                <button onClick={()=>handleTabs('elementInput')} id="btn-task" className='btn-tasks'>My Tasks</button>
+                <button onClick={()=>handleTabs('elementTextArea')} id="btn-note" className='btn-notes'>My Notes</button>
         </div>
 
-        { todoList.map((task) => (
-            <div  className='note-item'>
-                  <VisualizedNotes keyValue = {task.id} noteVlaue ={task.task} onClickTriger ={()=>visualizeToUpdate(task.id)} className = {'note-item-content'} />
-                  <DeleteButton onClickTriiger={()=> deleteTodo(task.id)} className={'btn-delete'} />
-            </div> 
-            ))}
+          <MapedComponents todoList={todoList} inputType={inputType} triggers={[visualizeToUpdate,deleteTodo]}/>
+
         </div>
     </>
   );
