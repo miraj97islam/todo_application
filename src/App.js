72b7506c5ef5563f508746a3e86312454288ba45
MD MIRAJ ISLAM <miraz97islam@gmail.com>
2024-12-31 22:06:29 +0600

    underline of tabs fixed
    now tab btns having underline based on their state and onClick

--- src/App.js
+++ src/App.js
@@ -47,8 +47,12 @@ function DeleteButton({onClickTriger, className}){
   return (  
         <button onClick={onClickTriger}  className= {className}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-           class="size-4 lg:size-5 ">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+           class="size-4 lg:size-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 
+                1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 
+                0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 
+                0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 
+                48.667 0 0 0-7.5 0" />
           </svg>
         </button>
   )
@@ -98,9 +102,6 @@ function App() {
       localStorage.setItem("todoList", JSON.stringify(updateList));
   
       setTodoList(updateList);
-
-    
-    
       setnewInput("");
     }    
   }
@@ -123,6 +124,7 @@ function App() {
 
     setTodoList(updateList);
     setnewInput("");
+    setEditTaskId(null);
   }
 
 
@@ -134,6 +136,8 @@ function App() {
   function handleTabs(typeOfInput){
 
       setinputType(typeOfInput);
+      setnewInput("");
+      setEditTaskId(null);
 
     }
 
@@ -155,8 +159,10 @@ function App() {
         </div>
 
          <div className='tab-buttons'>
-                <button onClick={()=>handleTabs('elementInput')} id="btn-task" className='btn-tasks'>My Tasks</button>
-                <button onClick={()=>handleTabs('elementTextArea')} id="btn-note" className='btn-notes'>My Notes</button>
+                <button onClick={()=>handleTabs('elementInput')} id="btn-task" 
+                    className={`'btn-tasks' ${inputType==='elementInput'? 'tab-btn-underline' : ''}`}>My Tasks</button>
+                <button onClick={()=>handleTabs('elementTextArea')} id="btn-note" 
+                    className={`'btn-notes' ${inputType==='elementTextArea'? 'tab-btn-underline' : ''}`}>My Notes</button>
         </div>
 
           <MapedComponents todoList={todoList} inputType={inputType} triggers={[visualizeToUpdate,deleteTodo]}/>
